5d9edbba7a73ab624bae9e45375ae4d5ac7502b4
Aitak Assilem <aitakassilem@dy085055.univ-rennes1.fr>
2025-10-13 10:36:32 +0200

    Ajout de la fonction AmplitudeModale

--- AmplitudeModale.m
+++ AmplitudeModale.m
@@ -1,1 +1,8 @@
-function [an,bn]=AmplitudeModale(L,el,kn,wn,n,H,Aff)
+% % Amplitude modale
+% an=2*H./(n*pi)*L/(L-el).*sin(kn*el)./(kn*el);
+% bn=zeros(size(n));
+
+function [an, bn] = AmplitudeModale(H, L, el, n, kn)
+    an = 2 * H ./ (n * pi) .* (L / (L - el)) .* sin(kn * el) ./ (kn * el);
+    bn = zeros(size(n));
+end
