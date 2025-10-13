a8fbb0cae42319d921c60355ab4e099e45c1da7a
Aitak Assilem <aitakassilem@dy085055.univ-rennes1.fr>
2025-10-13 10:16:01 +0200

    Ajout de la fonction DomaineModal pour le calcul des paramètres modaux

--- DomaineModal.m
+++ DomaineModal.m
@@ -1,1 +1,23 @@
-function [n,kn,wn,Lamb,Per,Freq]=DomaineModal(Nw,L,C)
+
+% % Domaine modal
+% nmax=10;        % Nombre maximal de mode considere      
+% n=(1:nmax)';    % Indices modaux
+% Nw=nmax;
+% kn=n*pi/L;      % Nombres d'ondes [1/m] : corde fixee aux deux extremites
+% wn=C*kn;        % Pulsation [rad/s], relation de dispersion
+% Lamb=2*pi./kn;  % Longueur d'onde de chaque mode [m]
+% Per=2*pi./wn;   % Periode de chaque mode [s]
+% Freq=1./Per;    % Frequence de chaque mode [Hz]
+
+
+
+function [n, kn, wn, Lamb, Per, Freq] = DomaineModal(L, C, nmax)
+    n = (1:nmax)';          
+    kn = n * pi / L;        
+    wn = C * kn;            
+    Lamb = 2 * pi ./ kn;    
+    Per = 2 * pi ./ wn;     
+    Freq = 1 ./ Per;        
+end
+
+
