9ba0427aa13b13a1d5a5b73fd0cdfe1ff48c6086
petit-asus-linux <frelonjeu@gmail.com>
2025-11-30 20:48:18 +0100

    N

--- Supplement/pression.m
+++ Supplement/pression.m
@@ -3,6 +3,6 @@ function p = pression(Pmicro,rho_air,c_air,A,kn,wn,Y,u,s)
   [theta,phi,r] = cart2sph([P 0]-[s 0 0]); %phi est toujours égal à 0 ici, donc e
   Du=derive(Y,an,bn,wn,t);
 
-  pn = ((wn.^2)*rho_air.*sqrt(A/pi)./(r.*4*pi*c_son)).*cos(theta).*(bn+i.*an).*exp(i.*(wn.*t-kn.*r)).*(1+1/(kn.*r));
+  pn = ((wn.^2)*rho_air.*sqrt(A/pi)./(r.*4*pi*c_son)).*cos(theta).*(bn+i.*an).*exp(i.*(wn.*(t-r./c_son))).*(1+c_son/(wn.*r));
   pn = partie_reele(pn);
 
